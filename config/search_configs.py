1ff362b2f6870635b2a081474299e31566aa14be
avivgelfandg <aviv.gelfand@gong.io>
2026-09-16 08:54:26 +0300

    change search configs

--- config/search_configs.py
+++ config/search_configs.py
@@ -1,90 +1,15 @@
 SEARCH_CONFIGURATIONS = [
-    {
-        "name": "Elevator",
+            {
+        "name": "most general search",
         "params": {
             "city": "6400",
             "minRooms": "3",
-            "maxRooms": "4.5",
+            "maxRooms": "4",
             "minPrice": "4500",
             "maxPrice": "8000",
-            "priceOnly": "1",
-            "elevator": "1",
-            "balcony": "1",
-            "renovated": "1"
-        }
-    },
-    {
-        "name": "Not Renovated",
-        "params": {
-            "city": "6400",
-            "minRooms": "3",
-            "maxRooms": "4.5",
-            "minPrice": "4500",
-            "maxPrice": "7000",
-            "imageOnly": "1",
-            "priceOnly": "1",
-            "elevator": "1",
-            "balcony": "1",
-        }
-    },
-        {
-        "name": "No Elevator",
-        "params": {
-            "city": "6400",
-            "minRooms": "3",
-            "maxRooms": "4.5",
-            "minPrice": "4500",
-            "maxPrice": "7000",
-            "priceOnly": "1",
-            "balcony": "1",
-            "minFloor": "0",
-            "maxFloor": "1",
-            "renovated": "1"
-        }
-        },
-            {
-        "name": "5 Rooms No Elevator",
-        "params": {
-            "city": "6400",
-            "minRooms": "3",
-            "maxRooms": "5",
-            "minPrice": "5000",
-            "maxPrice": "7300",
-            "imageOnly": "1",
-            "priceOnly": "1",
-            "balcony": "1",
-            "minFloor": "0",
-            "maxFloor": "1",
-            "renovated": "1"
-        }
-    },
-        {
-        "name": "5 Rooms with Elevator",
-        "params": {
-            "city": "6400",
-            "minRooms": "3",
-            "maxRooms": "5",
-            "minPrice": "5000",
-            "maxPrice": "7500",
-            "imageOnly": "1",
-            "priceOnly": "1",
-            "balcony": "1",
-            "renovated": "1",
-            "elevator": "1",
-        }
-    },
-            {
-        "name": "No Balcony with Elevator",
-        "params": {
-            "city": "6400",
-            "minRooms": "3",
-            "maxRooms": "5",
-            "minPrice": "4500",
-            "maxPrice": "7000",
             "imageOnly": "1",
             "priceOnly": "1",
             "renovated": "1",
-            "elevator": "1",
         }
     },
 ]
